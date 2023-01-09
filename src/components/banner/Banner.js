d5cebef58eb73efc7715ce4f431998dca2996f94
iamjayy <jason_doan94@yahoo.com>
2023-01-09 16:42:00 -0500

    use state and use effect for header

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,8 +1,49 @@
+import { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../../assets/img/header-img.svg";
 
 export const Banner = () => {
+  // state for typed text animation //
+  const [loopNum, setLoopNum] = useState(0); //useState to display the first index
+  const [isDeleting, setIsDeleting] = useState(false); //initially set to false because we start by typing the word
+  const toRotate = ["Web Developer", "Designer"];
+  const [text, setText] = useState(""); //indicate the portion of the text being displayed until full word
+  const [delta, setDelta] = useState(300 - Math.random() * 100); // determine how fast of one letter is typed
+  const period = 2000; //time of word being typed out
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length; // i = index from the array because the loop number '%' is constantly increasing and to rotate length
+    let fullText = toRotate[i];
+    let updatedText = isDeleting //text to be updated or set to.
+      ? fullText.substring(0, text.length - 1) //if current state if being deleting then take the full text and get its substring one letter less.
+      : fullText.substring(0, text.length + 1); //if not deleting then add one letter to the current text and come closer to the full text
+
+    setText(updatedText);
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
   return (
     <section className="banner" id="home">
       <Container>
@@ -10,8 +51,8 @@ export const Banner = () => {
           <Col xs={12} md={6} xl={7}>
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>
-              {"Hi I'am Jason"}
-              <span className="wrap">Web Developer</span>
+              {"Hi I'am Jason "}
+              <span className="wrap">{text}</span>
             </h1>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
